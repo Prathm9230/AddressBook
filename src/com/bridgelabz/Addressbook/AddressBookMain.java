9b9e073832a04bdf21d5239774c847384998094a
Prathm <jawahireprathmesh@gmail.com>
2023-06-30 23:02:51 +0530

    Added contact

--- src/com/bridgelabz/Addressbook/AddressBookMain.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.Addressbook;
-
-public class AddressBookMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome! to Address Book Program.");
-    }
-}
